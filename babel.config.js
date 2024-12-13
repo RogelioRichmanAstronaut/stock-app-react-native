bdaf8df91498286440fd7ed8e8b489d70eceaba3
RogelioRichmanAstronaut <dvsandoval@javeriana.edu.co>
2024-12-13 10:36:06 -0500

    fix alias

--- babel.config.js
+++ babel.config.js
@@ -10,10 +10,10 @@ module.exports = function (api) {
       [
         'module-resolver',
         {
-          root: ['./src'],
+          root: ['./'],
           extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
           alias: {
-            '@': './src'
+            '@': './'
           }
         }
       ]
